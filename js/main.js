dd3617b1f6f36bf108dece5b84b9fb00475cc25e
bumsuu5 <mr1850@naver.com>
2025-10-14 19:25:42 +0900

    initial project

--- js/main.js
+++ js/main.js
@@ -72,8 +72,8 @@
 	    var href = $.attr(this, 'href');
 
 	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
-	    }, 500, function() {
+	        scrollTop: $($.attr(this, 'href')).offset().top - 80
+	    }, 700, function() {
 	    	// window.location.hash = href;
 	    });
 		});
